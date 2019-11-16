bf2ae3c5e73323298f54ef1c32198d20e1d195a1
marksanghoonkim <mark.s.kim@gmail.com>
2019-11-15 17:04:40 -0800

    complete q3iii - q4iv

--- hw1.sql
+++ hw1.sql
@@ -102,31 +102,77 @@ AS
 -- Question 3iii
 CREATE VIEW q3iii(namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1 -- replace this line
-;
+	WITH lifetimeslugging(playerid, namefirst, namelast, lslg) AS
+		(SELECT b.playerid, p.namefirst, p.namelast, (
+			CAST((SUM(b.h2b) * 2 + SUM(h3b) * 3 + SUM(hr) * 4 + (SUM(h) - SUM(h2b) - SUM(h3b) - SUM(hr))) AS float) /
+			CAST(SUM(ab) AS float) ) AS lifeslug
+		FROM batting b, people p
+		WHERE ab > 0 AND b.playerid = p.playerid
+		GROUP BY b.playerid, p.playerid
+		HAVING SUM(ab) >= 50)
 
+	SELECT namefirst, namelast, lslg
+	FROM lifetimeslugging
+	WHERE lslg > (
+		SELECT lslg
+		FROM lifetimeslugging
+		WHERE playerid = 'mayswi01'
+	)
+	ORDER BY namefirst, namelast;
+;
 -- Question 4i
 CREATE VIEW q4i(yearid, min, max, avg, stddev)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+	SELECT yearid, MIN(salary), MAX(salary), AVG(salary), STDDEV(salary) FROM Salaries GROUP BY yearid ORDER BY yearid;
 ;
 
 -- Question 4ii
 CREATE VIEW q4ii(binid, low, high, count)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+
+WITH salaries_stat AS (
+	SELECT min(salary) AS min_2016, max(salary) AS max_2016, (max(salary) - min(salary))/10 AS range_2016
+	FROM salaries
+	WHERE yearid = '2016'
+	),
+	bin AS (
+	SELECT width_bucket(salary, min_2016, max_2016 + 1, 10) - 1 as binid, count(*) as count
+	FROM salaries, salaries_stat
+	WHERE yearid = '2016'
+	GROUP BY binid
+	)
+SELECT binid, min_2016+range_2016*binid as min, min_2016+range_2016*(binid+1) as max, count
+FROM bin, salaries_stat
+ORDER BY binid;
+
 ;
 
 -- Question 4iii
 CREATE VIEW q4iii(yearid, mindiff, maxdiff, avgdiff)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+
+SELECT cur.yearid, cur.min - pre.min, cur.max - pre.max, cur.avg - pre.avg
+FROM q4i AS cur
+INNER JOIN q4i AS pre
+ON cur.yearid - 1 = pre.yearid
+ORDER BY cur.yearid
+
 ;
 
 -- Question 4iv
 CREATE VIEW q4iv(playerid, namefirst, namelast, salary, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+
+SELECT p.playerid, namefirst, namelast, salary, yearid
+FROM people AS p
+INNER JOIN salaries AS s
+ON p.playerid = s.playerid
+WHERE (yearid BETWEEN 2000 AND 2001) AND (yearid, salary) IN (
+	SELECT yearid, MAX(salary)
+	FROM salaries
+	GROUP BY yearid
+)
+
 ;
 -- Question 4v
 CREATE VIEW q4v(team, diffAvg) AS
